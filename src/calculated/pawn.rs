fc35488881cbf596b1fc72b80d729af0f1f43679
Joel Milligan <joelmilligan24@gmail.com>
2023-07-09 00:27:51 +0800

    Split up pawn tests

--- src/calculated/pawn.rs
+++ src/calculated/pawn.rs
@@ -31,8 +31,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn pawn_attacks() {
-        // No results in first or last rank
+    fn full_move_set() {
+        assert_eq!(PAWN_ATTACKS[WHITE][E4], 0x2800000000);
+        assert_eq!(PAWN_ATTACKS[BLACK][D5], 0x14000000);
+    }
+
+    #[test]
+    fn white_edges() {
+        assert_eq!(PAWN_ATTACKS[WHITE][A2], 0x20000);
+        assert_eq!(PAWN_ATTACKS[WHITE][H2], 0x400000);
+        assert_eq!(PAWN_ATTACKS[WHITE][A7], 0x200000000000000);
+        assert_eq!(PAWN_ATTACKS[WHITE][H7], 0x4000000000000000);
+    }
+
+    #[test]
+    fn black_edges() {
+        assert_eq!(PAWN_ATTACKS[BLACK][A2], 0x2);
+        assert_eq!(PAWN_ATTACKS[BLACK][H2], 0x40);
+        assert_eq!(PAWN_ATTACKS[BLACK][A7], 0x20000000000);
+        assert_eq!(PAWN_ATTACKS[BLACK][H7], 0x400000000000);
+    }
+
+    #[test]
+    fn end_of_board() {
         assert_eq!(PAWN_ATTACKS[WHITE][A1], 0);
         assert_eq!(PAWN_ATTACKS[WHITE][H1], 0);
         assert_eq!(PAWN_ATTACKS[WHITE][A8], 0);
@@ -42,20 +63,5 @@ mod tests {
         assert_eq!(PAWN_ATTACKS[BLACK][H1], 0);
         assert_eq!(PAWN_ATTACKS[BLACK][A8], 0);
         assert_eq!(PAWN_ATTACKS[BLACK][H8], 0);
-
-        // Check edges
-        assert_eq!(PAWN_ATTACKS[WHITE][A2], 0x20000);
-        assert_eq!(PAWN_ATTACKS[WHITE][H2], 0x400000);
-        assert_eq!(PAWN_ATTACKS[WHITE][A7], 0x200000000000000);
-        assert_eq!(PAWN_ATTACKS[WHITE][H7], 0x4000000000000000);
-
-        assert_eq!(PAWN_ATTACKS[BLACK][A2], 0x2);
-        assert_eq!(PAWN_ATTACKS[BLACK][H2], 0x40);
-        assert_eq!(PAWN_ATTACKS[BLACK][A7], 0x20000000000);
-        assert_eq!(PAWN_ATTACKS[BLACK][H7], 0x400000000000);
-
-        // Normal moves
-        assert_eq!(PAWN_ATTACKS[WHITE][E4], 0x2800000000);
-        assert_eq!(PAWN_ATTACKS[BLACK][D5], 0x14000000);
     }
 }
